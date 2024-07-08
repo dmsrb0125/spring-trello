7da806d24ee0a20d174e79a0be6979d1ac9b49f2
dmsrb0125 <52693374+dmsrb0125@users.noreply.github.com>
2024-07-09 02:03:19 +0900

    refactor : 정렬 수정

--- src/main/java/com/sparta/springtrello/exception/custom/user/UserNotFoundException.java
+++ src/main/java/com/sparta/springtrello/exception/custom/user/UserNotFoundException.java
@@ -2,6 +2,8 @@ package com.sparta.springtrello.exception.custom.user;
 
 import com.sparta.springtrello.common.ResponseCodeEnum;
 
-public class UserNotFoundException extends UserException{
-    public UserNotFoundException(){ super(ResponseCodeEnum.USER_NOT_FOUND);}
+public class UserNotFoundException extends UserException {
+    public UserNotFoundException() {
+        super(ResponseCodeEnum.USER_NOT_FOUND);
+    }
 }
